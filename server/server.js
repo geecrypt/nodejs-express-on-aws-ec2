3a8e49916d3faa68b2f4223bd3202cebe1739b6b
geecrypt <gavina352@gmail.com>
2021-10-05 10:20:54 -0700

    connect react to server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 require('dotenv').config();
 const { Pool } = require('pg');
 const app = express();
-const port = 3000;
+const port = 8081;
 
 let dbParams = {};
 if (process.env.DATABASE_URL) {
@@ -24,15 +24,15 @@ app.get("/", (req, res) => {
   res.send("<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.3</p>");
 });
 
-app.get("/products", (req, res) => {
+app.get("/api/products", (req, res) => {
   db.query('SELECT * FROM products')
     .then((data) => {
       console.log(data.rows)
-      res.send(data.rows)
+      res.json({data: data.rows})
     })
     .catch((e) => {
       console.log('Error:', e);
-      res.send('DB Error')
+      res.json({error: 'DB Error'})
     })
 });
 
--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/products')
+      .then((res) => {
+        console.log('res', res.data);
+        setData(res.data.data)
+      })
+      .catch((e) => console.log('error /api/products GET', e));
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {data.map((d) => (
+            <li key={d.id}>{d.name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
